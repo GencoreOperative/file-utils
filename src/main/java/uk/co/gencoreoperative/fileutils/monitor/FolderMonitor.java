700d7e3ec8323c71c7ea7d3d1473532d47d48119
Robert Wapshott <robert@wapshott.org.uk>
2014-05-12 13:48:49 +0100

    Disabling the FileMonitor and removing dependency on Lookup library.

--- src/main/java/uk/co/gencoreoperative/fileutils/monitor/FolderMonitor.java
+++ src/main/java/uk/co/gencoreoperative/fileutils/monitor/FolderMonitor.java
@@ -15,71 +15,64 @@
  */
 package uk.co.gencoreoperative.fileutils.monitor;
 
-import simplelookup.Lookup;
-
-import java.io.File;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 /**
  * FileMonitor is reponsible for managing the polling process and providing the caller
  * with the option to customise the polling process.
  */
 public class FolderMonitor {
-    private File folder;
-    private int duration = 5;
-    private boolean started = false;
-    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-    private Lookup lookup;
-
-    public FolderMonitor monitor(File folder) {
-        this.folder = folder;
-        return this;
-    }
-
-    public FolderMonitor every(int seconds) {
-        duration = seconds;
-        return this;
-    }
-
-    /**
-     * Define the Lookup which will be used for signalling file changes.
-     * @param lookup Non null lookup.
-     * @return The FolderMonitor instance.
-     */
-    public FolderMonitor usingLookup(Lookup lookup) {
-        this.lookup = lookup;
-        return this;
-    }
-
-    public Lookup start() {
-        if (folder == null) {
-            throw new IllegalStateException("Folder must be assigned");
-        }
-
-        if (started) {
-            throw new IllegalStateException("Monitor is already started");
-        }
-
-        if (lookup == null) {
-            lookup = new Lookup();
-        }
-
-        Runnable poller = new Runnable() {
-            private FilePoller filePoller = new FilePoller(folder);
-            private Lookup.View<FileEvent> view = lookup.getView(FileEvent.class);
-            @Override
-            public void run() {
-                view.replaceAllWith(filePoller.poll());
-            }
-        };
-
-        executorService.scheduleAtFixedRate(poller, 0l, (long)duration, TimeUnit.SECONDS);
-        return lookup;
-    }
-
-    public void stop() {
-        executorService.shutdown();
-    }
+//    private File folder;
+//    private int duration = 5;
+//    private boolean started = false;
+//    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+//    private Lookup lookup;
+//
+//    public FolderMonitor monitor(File folder) {
+//        this.folder = folder;
+//        return this;
+//    }
+//
+//    public FolderMonitor every(int seconds) {
+//        duration = seconds;
+//        return this;
+//    }
+//
+//    /**
+//     * Define the Lookup which will be used for signalling file changes.
+//     * @param lookup Non null lookup.
+//     * @return The FolderMonitor instance.
+//     */
+//    public FolderMonitor usingLookup(Lookup lookup) {
+//        this.lookup = lookup;
+//        return this;
+//    }
+//
+//    public Lookup start() {
+//        if (folder == null) {
+//            throw new IllegalStateException("Folder must be assigned");
+//        }
+//
+//        if (started) {
+//            throw new IllegalStateException("Monitor is already started");
+//        }
+//
+//        if (lookup == null) {
+//            lookup = new Lookup();
+//        }
+//
+//        Runnable poller = new Runnable() {
+//            private FilePoller filePoller = new FilePoller(folder);
+//            private Lookup.View<FileEvent> view = lookup.getView(FileEvent.class);
+//            @Override
+//            public void run() {
+//                view.replaceAllWith(filePoller.poll());
+//            }
+//        };
+//
+//        executorService.scheduleAtFixedRate(poller, 0l, (long)duration, TimeUnit.SECONDS);
+//        return lookup;
+//    }
+//
+//    public void stop() {
+//        executorService.shutdown();
+//    }
 }
